8763c15271e1addb7bf9def2ea0dcde6cd7f50ad
Adolfo Correa <fitomillennium@hotmail.com>
2015-03-20 14:40:09 -0300

    Helps
    Foram incluidos os helps de cada função

--- aula1.py
+++ aula1.py
@@ -1,26 +1,32 @@
 # -*- coding: cp1252 -*-
 def soma(x,y):
-	return x+y
+    """Soma dos numeros"""
+    return x+y
 
 def raiz(a):
-	return a**(1.0/2.0)
+    """Raiz quadrada de a"""
+    return a**(1.0/2.0)
 
 def subtra(x,y):
-	return x-y
+    """Raiz quadrada de a"""
+    return x-y
 
 def multip(x,y):
+        """Produto de dois numero"""
         return x*y
 
 def div(x,y):
-        """ayuda"""
+        """Divisão de x/y"""
         x=float(x)
         y=float(y)
         return x/y
 
 def quadrado(a):
-        return a**2
+    """Quadrado de a"""
+    return a**2
 
 def area():
+        """Area de um retangulo"""
         print 'a='
         a=input()
         print 'b='
@@ -30,32 +36,39 @@ def area():
 
 #Essa função vai calcular a área de um retângulo
 def retangulo(a,b):
+        """Area de um retangulo"""
         return a*b
 
 #Essa função vai calcular a área de uma coroa circular
 def coroa(r1,r2):
+        """Area da coroa"""
         pi=3.14
         area=pi*(r1**2-r2**2)
         return area
 
 #Divisão de 2 nros inteiros
 def div2(x,y):
+        """Cociente e resto"""
         return x/y,x%y
 
 #Ordenada de uma função do segundo grau
 def ordenada(a,b,c,x):
+        """Ordenada de uma parabola"""
         return a*x**2+b*x+c
 
 #Entra o valor da conta e devolve a gorjeta
 def gorjeta(a):
+        """Gorjeta do garcao"""
         return a*0.1
 
 #Média de dois valores
 def media(a,b):
+        """Media de 2 numeros"""
         return (a+b)/2
 
 
 #Média ponderada de dois valores
 def mediapon(a,p1,b,p2):
+        """Media ponderada de 2 numeros"""
         p1=float(p1)
         return (a*p1+b*p2)/(p1+p2)

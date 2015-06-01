acb7535279d28b42c4a99e2078b87a7ab432e931
Adolfo Correa <fitomillennium@hotmail.com>
2015-06-01 03:17:10 -0300

    9 Aula practica Exc2
    Bubble Sort Method

--- 6aula_pratica.py
+++ 6aula_pratica.py
@@ -26,6 +26,7 @@ def inv_phrase(phrase):
 #    return sort_list
     
 def select_sort(lista):
+    "Select Sort Method"
     for index, elem in enumerate(lista):
         menor = elem
         for j_esimo, elem in enumerate(lista[index:]):
@@ -36,10 +37,24 @@ def select_sort(lista):
         lista[index] = menor
     return lista
     
+def bubble_sort(lista):
+    "Bubble Sort Method"
+    flag = True
+    while flag:
+        flag = False
+        for index in range(len(lista) - 1):
+        #for index in range(len(lista)):
+            if lista[index + 1] < lista[index]:
+                aux = lista[index]
+                lista[index] = lista[index + 1]
+                lista[index + 1] = aux
+                flag = True
+    return lista
     
 
-if __name__=='__main__':
+if __name__ == '__main__':
     print (inv_phrase("eu gosto de doce"))
     lista = [9,8,4,5,1,9,8,6,4,8,9,2,3]
     print (lista)
     print (select_sort(lista))
+    print (bubble_sort(lista))

2fd19f9f658d6747cb81de857bbb814a63e4d813
Adolfo Correa <fitomillennium@hotmail.com>
2015-06-17 16:32:45 -0300

    10 aula Romanian numbers
    Correction.
    Now function can write romanian numbers since 0 to 3000

--- 10aula.py
+++ 10aula.py
@@ -39,19 +39,22 @@ def dict_wo_duplicates(lista):
 
 # Exercise 2
 def romanian_numbers(num):
+    "Return a string with the romanian number from 1 to 3000"
     UNIDADES = { 0: '', 1: 'I', 2: 'II', 3: 'III', 4: 'IV', 5: 'V', 6: 'VI',
                  7: 'VII', 8: 'VIII', 9: 'IX' }
     DEZENAS = { 0: '', 1: 'X', 2: 'XX', 3: 'XXX', 4: 'XL', 5: 'L', 6: 'LX',
                 7: 'LXX', 8: 'LXXX', 9: 'XC' }
-    CENTENAS = { 0: '', 2: 'C', 2: 'CC', 3: 'CCC', 4: 'CD', 5: 'D', 6: 'DC',
-                 7: 'DCC', 8:'DCCC', 9:'CM' } 
+    CENTENAS = { 0: '', 1: 'C', 2: 'CC', 3: 'CCC', 4: 'CD', 5: 'D', 6: 'DC',
+                 7: 'DCC', 8:'DCCC', 9:'CM' }
+    MILHARES = { 0: '', 1: 'M', 2: 'MM', 3: 'MMM'} 
+    
     num_list = []
     num_list.extend(str(num))
     ##if len(num_list) == 2:
     ##rom_num_list = DEZENAS[int(num_list)]
     lista = []
     i = -1
-    for base in (UNIDADES, DEZENAS, CENTENAS):
+    for base in (UNIDADES, DEZENAS, CENTENAS, MILHARES):
         lista.append(base[int(num_list[i])])
         if abs(i) == len(num_list): break
         i -= 1
@@ -77,3 +80,5 @@ if __name__ == '__main__':
     print(romanian_numbers(13))
     print(romanian_numbers(19))
     print(romanian_numbers(999))
+    print(romanian_numbers(2015))
+    print(romanian_numbers(3000))
